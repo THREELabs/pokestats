b42d236519775181e1df4bc2f413bf1d980f8b78
THREELabs <51836729+THREELabs@users.noreply.github.com>
2020-08-02 19:24:28 -0700

    Upload
    Main
    Styles
    Index

---
+++ main.js
@@ -0,0 +1,34 @@
+const apiData = {
+	url: 'https://pokeapi.co/api/v2/',
+	type: 'pokemon',
+	id: '180',
+}
+const {url, type, id} = apiData
+
+const apiUrl = `${url}${type}/${id}`
+
+fetch(apiUrl)
+	.then( (data) => {
+			if(data.ok){
+				return data.json()
+			}
+			throw new Error('Response failed m(-_-)m');
+	})
+	
+	
+	.then( (pokemon) => generateHtml(pokemon))
+
+const generateHtml = (data) => {
+	console.log(data)
+
+const html = `
+	<div class="name">${data.name}</div>
+	<img src=${data.sprites.front_default}>
+	<div class="details">
+		<span class="spec">Height: ${data.height}</span>
+		
+		<span class="spec">Weight: ${data.weight}</span>
+	</div>`
+
+const pokemonDiv = document.querySelector('.pokemon')
+	pokemonDiv.innerHTML = html}
